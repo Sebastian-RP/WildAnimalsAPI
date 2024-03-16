9719715815c0db4c87429ebd9e0ed8ae0035a9b6
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2024-03-16 13:34:53 -0500

    creation of animal and contient models

---
+++ WildAnimalsAPI/Models/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace WildAnimalsAPI.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string CommonName { get; set; }
+        public string ScientificName { get; set; }
+        public int AverageHeightMin { get; set; }
+        public int AverageHeightMax { get; set; }
+        public string Description { get; set; }
+        public int AverageSpeedMin { get; set; }
+        public int AverageSpeedMax { get; set; }
+        public int AverageWeightMin { get; set; }
+        public int AverageWeightMax { get; set; }
+        public int EstimatedPopulationSize { get; set; }
+        public int AverageLifespanMin { get; set; }
+        public int AverageLifespanMax { get; set; }
+        public string Diet { get; set; }
+    }
+}
---
+++ WildAnimalsAPI/Models/Continent.cs
@@ -0,0 +1,10 @@
+﻿namespace WildAnimalsAPI.Models
+{
+    public class Continent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public int SurfaceKm2 { get; set; }
+    }
+}
